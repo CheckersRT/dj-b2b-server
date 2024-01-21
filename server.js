3ffc6a70d2938eccfdd3fe54b744bfd33bc01bb4
Tom Dent <tomdent991@hotmail.com>
2024-01-21 23:36:29 +0100

    adding xml upload

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import * as http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 import jsmediatags from "jsmediatags";
+import {xml2js} from "xml-js"
+import { readFileSync } from "fs";
+
 
 const app = express();
 const PORT = process.env.PORT || 3030;
@@ -58,12 +61,14 @@ app.post("/metadata", (request, response) => {
   jsmediatags.read(request.body.url, {
     onSuccess: function (tag) {
       const tags = tag.tags;
+      console.log(tag)
       //Create a buffer, stringify it and format it to a data url.
       const art = `data:${tags.picture.format};base64,${Buffer.from(
         tags.picture.data
       ).toString("base64")}`;
 
       response.status(200).json({
+        url: request.body.url,
         artist: tags.artist,
         album: tags.album,
         title: tags.title,
@@ -77,6 +82,17 @@ app.post("/metadata", (request, response) => {
   });
 });
 
+
+var xml = readFileSync('/Users/Checkers/Documents/spiced/dj-b2b-server/public/rekordbox.xml', 'utf8');
+var options = {ignoreComment: true, alwaysChildren: true};
+var result = xml2js(xml, options); // or convert.xml2json(xml, options)
+var element = result.elements.forEach((element) => {
+  console.log("elements:", element)
+})
+element.forEach((element) => {
+  // element.elements
+})
+// console.log(result);
 // const result = getMetaDataFromFile(trackUri);
 // console.log("Result: ", result);
 
