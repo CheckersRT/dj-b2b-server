2f8a98df7f87fba25f6848718bfe2b634b7bb650
Tom Dent <tomdent991@hotmail.com>
2024-02-04 21:39:06 +0100

    changed cors config

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://dj-b2b-client.vercel.app" ,
     methods: ["GET", "POST"],
   },
 });
